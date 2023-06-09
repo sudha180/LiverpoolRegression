4aade9f2bf75d41d51875b21d4623434e7c72492
sudha180 <122967083+sudha180@users.noreply.github.com>
2023-06-09 20:17:54 +0530

    changes

--- Object Repository/DemoObjects/PLPPage/div_Vendido por_plp.rs
+++ Object Repository/DemoObjects/PLPPage/div_Vendido por_plp.rs
@@ -6,12 +6,11 @@
    <elementGuidId>d5220db6-f3fe-489a-805f-a25d4f35cb77</elementGuidId>
    <selectorCollection>
       <entry>
-         <key>XPATH</key>
-         <value>//div[@class='col a-text__filter']//label[contains(text(),'Vendido por')]</value>
+         <key>CSS</key>
       </entry>
       <entry>
-         <key>CSS</key>
-         <value></value>
+         <key>XPATH</key>
+         <value>//div[@class='col a-text__filter']//label[contains(text(),'Vendido')]</value>
       </entry>
    </selectorCollection>
    <selectorMethod>XPATH</selectorMethod>
@@ -22,7 +21,7 @@
       <name>tag</name>
       <type>Main</type>
       <value>div</value>
-      <webElementGuid>8d12bfec-ac31-4304-b454-c737577dca50</webElementGuid>
+      <webElementGuid>747cb0d3-ab69-4409-8957-b40cc7cc8687</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -30,7 +29,7 @@
       <name>class</name>
       <type>Main</type>
       <value>col a-text__filter</value>
-      <webElementGuid>9ee6555e-df51-45c9-9488-546198a26dd6</webElementGuid>
+      <webElementGuid>f50542eb-4841-4879-9f78-24e37589de99</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>true</isSelected>
@@ -38,7 +37,7 @@
       <name>text</name>
       <type>Main</type>
       <value>Vendido por</value>
-      <webElementGuid>fefe495a-445c-428d-b787-50108006328f</webElementGuid>
+      <webElementGuid>06e887a1-fc77-4084-bb09-5ba496b56f58</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -46,7 +45,7 @@
       <name>xpath</name>
       <type>Main</type>
       <value>id(&quot;__next&quot;)/div[@class=&quot;min-vh-100&quot;]/div[@class=&quot;container-fluid o-main-container p-0 o-plp-secondaryContainer&quot;]/div[@class=&quot;container o-container__secondary search---&quot;]/div[@class=&quot;row mt-lg-3 m-row-bootstrap&quot;]/aside[@class=&quot;col-lg-3 pr-4 m-aside__content&quot;]/div[@class=&quot;m-plp-cat-container d-none d-lg-block mb-5&quot;]/div[@class=&quot;o-aside d-aside--margin&quot;]/div[@class=&quot;m-plp__filterSection&quot;]/div[@class=&quot;a-plp__btnFilter&quot;]/button[@class=&quot;a-title__filter&quot;]/h3[@class=&quot;m-0&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col a-text__filter&quot;]</value>
-      <webElementGuid>dd9ac6eb-6110-476d-be8e-e8aa70c05591</webElementGuid>
+      <webElementGuid>937aae0d-4d54-4ff9-a415-120d610d05f3</webElementGuid>
    </webElementProperties>
    <webElementXpaths>
       <isSelected>true</isSelected>
@@ -54,7 +53,7 @@
       <name>xpath:idRelative</name>
       <type>Main</type>
       <value>//div[@id='__next']/div/div/div/div[4]/aside/div/div/div[29]/div/button/h3/div/div</value>
-      <webElementGuid>1311e7de-7326-4728-ab9d-70dcbc7f8346</webElementGuid>
+      <webElementGuid>6be36d05-1fc2-4e5f-8320-217a3177b56b</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -62,7 +61,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='-'])[1]/following::div[7]</value>
-      <webElementGuid>98d6fd4e-1d07-422c-b277-0567850a11af</webElementGuid>
+      <webElementGuid>b331daec-589c-4d32-9618-8c6a8aa9bf25</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -70,7 +69,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='Liverpool (1101)'])[1]/preceding::div[5]</value>
-      <webElementGuid>1c2e3f84-de43-48d9-8ee7-6ab32648e0ac</webElementGuid>
+      <webElementGuid>a686a41a-1b21-4140-a945-537de7dcbe74</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -78,7 +77,7 @@
       <name>xpath:position</name>
       <type>Main</type>
       <value>//div[29]/div/button/h3/div/div</value>
-      <webElementGuid>8a67e74f-4ae9-42c5-ac94-9d4684ef7b72</webElementGuid>
+      <webElementGuid>83186627-9a4d-40a3-ba4a-1f96a59204d0</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -86,6 +85,6 @@
       <name>xpath:customAttributes</name>
       <type>Main</type>
       <value>//div[(text() = 'Vendido por' or . = 'Vendido por')]</value>
-      <webElementGuid>b8fcf957-2e47-4f9e-bf9d-c9780b890dd9</webElementGuid>
+      <webElementGuid>5fcc7078-8b16-4f6a-9ef8-e2991d338da8</webElementGuid>
    </webElementXpaths>
 </WebElementEntity>
---
+++ Object Repository/PLPPage/goToTop.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>goToTop</name>
+   <tag></tag>
+   <elementGuidId>407b5790-085f-4deb-ace5-0d6cc7045355</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='goToTop']//i</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
